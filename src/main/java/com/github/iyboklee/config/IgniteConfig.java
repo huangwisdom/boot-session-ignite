9ff9c43b6d1f27a122cae2972dd49f096e18a9e1
iyboklee <iyboklee@gmail.com>
2018-08-12 03:09:05 +0900

    정리

--- src/main/java/com/github/iyboklee/config/IgniteConfig.java
+++ src/main/java/com/github/iyboklee/config/IgniteConfig.java
@@ -8,6 +8,7 @@ import org.apache.ignite.Ignition;
 import org.apache.ignite.cache.CacheMode;
 import org.apache.ignite.cache.eviction.lru.LruEvictionPolicyFactory;
 import org.apache.ignite.configuration.CacheConfiguration;
+import org.apache.ignite.configuration.DataRegionConfiguration;
 import org.apache.ignite.configuration.DataStorageConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.logger.slf4j.Slf4jLogger;
@@ -35,7 +36,7 @@ public class IgniteConfig {
 
         // Cluster Discovery
         TcpDiscoverySpi spi  = new TcpDiscoverySpi();
-        TcpDiscoveryVmIpFinder ipFinder  = new TcpDiscoveryVmIpFinder();
+        TcpDiscoveryVmIpFinder ipFinder = new TcpDiscoveryVmIpFinder();
         ipFinder.setAddresses(nodes);
         spi.setIpFinder(ipFinder);
         spi.setSocketTimeout(100);
@@ -50,9 +51,10 @@ public class IgniteConfig {
 
         // Memory Configuration
         DataStorageConfiguration storageCfg = new DataStorageConfiguration();
-        storageCfg.getDefaultDataRegionConfiguration().setPersistenceEnabled(false);   // Only Memory
-        storageCfg.getDefaultDataRegionConfiguration().setMaxSize(1024 * 1024 * 256);  // 256MB
-        storageCfg.getDefaultDataRegionConfiguration().setMetricsEnabled(true);
+        DataRegionConfiguration defaultDataRegionCfg = storageCfg.getDefaultDataRegionConfiguration();
+        defaultDataRegionCfg.setPersistenceEnabled(false);   // Only Memory
+        defaultDataRegionCfg.setMaxSize(1024 * 1024 * 256);  // 256MB
+        defaultDataRegionCfg.setMetricsEnabled(true);
         igniteCfg.setDataStorageConfiguration(storageCfg);
 
         // Cache Configuration
